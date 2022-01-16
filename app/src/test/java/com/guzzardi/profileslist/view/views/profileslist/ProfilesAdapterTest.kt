cf05b8c8000ff507096350bd7bdb0ef8c9e276ac
Gonzalo Guzzardi <gonzaloguzzardi@gmail.com>
2022-01-16 01:21:30 -0300

    Fix tests

--- app/src/test/java/com/guzzardi/profileslist/view/views/profileslist/ProfilesAdapterTest.kt
+++ app/src/test/java/com/guzzardi/profileslist/view/views/profileslist/ProfilesAdapterTest.kt
@@ -4,13 +4,24 @@ import android.widget.FrameLayout
 import com.guzzardi.profileslist.RobolectricTest
 import com.guzzardi.profileslist.model.UserProfile
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 class ProfilesAdapterTest : RobolectricTest() {
 
+    @Mock
+    private lateinit var listener: ProfilesRecyclerView.OnProfileRemovedListener
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
     @Test
     fun `OnBindViewHolder binds user profile data to ProfileViewHolder`() {
-        val adapter = ProfilesAdapter()
+        val adapter = ProfilesAdapter(listener)
         val viewHolder = adapter.onCreateViewHolder(FrameLayout(context), 0)
 
         val data = UserProfile(GIVEN_NAME, FAMILY_NAME, "", null)
